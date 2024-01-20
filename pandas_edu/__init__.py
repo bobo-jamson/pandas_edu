eaf1f172bdf3410d0c6cd8c9803710bdb7c08c85
Bobo Jamson <bobo.jamson@gmail.com>
2024-01-20 14:40:41 -0600

    updated doc init since it shows up on pypi

--- pandas_edu/__init__.py
+++ pandas_edu/__init__.py
@@ -1,4 +1,4 @@
-"""pandas_edu top level imports"""
-__version__ = "0.1.0"
+"""pandas_edu provides a new pandas accessor (df.edu) with convenient methods for data exploration"""
+__version__ = "0.1.0b"
 
 from . import pandas_edu
